c1dc224843dd1807446b455d9fc9fe9ac7318b9f
PavelSadykov <2439747@gmail.com>
2023-12-25 14:24:06 +0500

    Добавьте файлы проекта.

---
+++ ProcessViewApp/Form1.Designer.cs
@@ -0,0 +1,202 @@
+﻿namespace ProcessViewApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBoxProcesses = new System.Windows.Forms.ListBox();
+            this.textBoxInterval = new System.Windows.Forms.TextBox();
+            this.btnSetInterval = new System.Windows.Forms.Button();
+            this.dataGridViewDetails = new System.Windows.Forms.DataGridView();
+            this.btnEndProcess = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnRunNotepad = new System.Windows.Forms.Button();
+            this.btnRunCalculator = new System.Windows.Forms.Button();
+            this.btnRunPaint = new System.Windows.Forms.Button();
+            this.btnRunCustomApp = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewDetails)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // listBoxProcesses
+            // 
+            this.listBoxProcesses.FormattingEnabled = true;
+            this.listBoxProcesses.Location = new System.Drawing.Point(12, 29);
+            this.listBoxProcesses.Name = "listBoxProcesses";
+            this.listBoxProcesses.Size = new System.Drawing.Size(274, 342);
+            this.listBoxProcesses.TabIndex = 0;
+            // 
+            // textBoxInterval
+            // 
+            this.textBoxInterval.Location = new System.Drawing.Point(685, 41);
+            this.textBoxInterval.Name = "textBoxInterval";
+            this.textBoxInterval.Size = new System.Drawing.Size(75, 20);
+            this.textBoxInterval.TabIndex = 1;
+            // 
+            // btnSetInterval
+            // 
+            this.btnSetInterval.Location = new System.Drawing.Point(685, 67);
+            this.btnSetInterval.Name = "btnSetInterval";
+            this.btnSetInterval.Size = new System.Drawing.Size(75, 23);
+            this.btnSetInterval.TabIndex = 2;
+            this.btnSetInterval.Text = "Enter";
+            this.btnSetInterval.UseVisualStyleBackColor = true;
+            this.btnSetInterval.Click += new System.EventHandler(this.btnSetInterval_Click_1);
+            // 
+            // dataGridViewDetails
+            // 
+            this.dataGridViewDetails.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewDetails.Location = new System.Drawing.Point(316, 29);
+            this.dataGridViewDetails.Name = "dataGridViewDetails";
+            this.dataGridViewDetails.Size = new System.Drawing.Size(360, 179);
+            this.dataGridViewDetails.TabIndex = 3;
+            // 
+            // btnEndProcess
+            // 
+            this.btnEndProcess.Location = new System.Drawing.Point(688, 146);
+            this.btnEndProcess.Name = "btnEndProcess";
+            this.btnEndProcess.Size = new System.Drawing.Size(75, 23);
+            this.btnEndProcess.TabIndex = 4;
+            this.btnEndProcess.Text = "Enter";
+            this.btnEndProcess.UseVisualStyleBackColor = true;
+            this.btnEndProcess.Click += new System.EventHandler(this.btnEndProcess_Click_1);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(682, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(117, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Установить интервал";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(685, 130);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(108, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Завершить процесс";
+            // 
+            // btnRunNotepad
+            // 
+            this.btnRunNotepad.Location = new System.Drawing.Point(316, 251);
+            this.btnRunNotepad.Name = "btnRunNotepad";
+            this.btnRunNotepad.Size = new System.Drawing.Size(116, 49);
+            this.btnRunNotepad.TabIndex = 7;
+            this.btnRunNotepad.Text = "Блокнот";
+            this.btnRunNotepad.UseVisualStyleBackColor = true;
+            this.btnRunNotepad.Click += new System.EventHandler(this.btnRunNotepad_Click_1);
+            // 
+            // btnRunCalculator
+            // 
+            this.btnRunCalculator.Location = new System.Drawing.Point(438, 251);
+            this.btnRunCalculator.Name = "btnRunCalculator";
+            this.btnRunCalculator.Size = new System.Drawing.Size(116, 49);
+            this.btnRunCalculator.TabIndex = 8;
+            this.btnRunCalculator.Text = "Калькулятор";
+            this.btnRunCalculator.UseVisualStyleBackColor = true;
+            this.btnRunCalculator.Click += new System.EventHandler(this.btnRunCalculator_Click_1);
+            // 
+            // btnRunPaint
+            // 
+            this.btnRunPaint.Location = new System.Drawing.Point(560, 251);
+            this.btnRunPaint.Name = "btnRunPaint";
+            this.btnRunPaint.Size = new System.Drawing.Size(116, 49);
+            this.btnRunPaint.TabIndex = 9;
+            this.btnRunPaint.Text = "Paint";
+            this.btnRunPaint.UseVisualStyleBackColor = true;
+            // 
+            // btnRunCustomApp
+            // 
+            this.btnRunCustomApp.Location = new System.Drawing.Point(438, 306);
+            this.btnRunCustomApp.Name = "btnRunCustomApp";
+            this.btnRunCustomApp.Size = new System.Drawing.Size(116, 49);
+            this.btnRunCustomApp.TabIndex = 10;
+            this.btnRunCustomApp.Text = "Пользовательское приложение";
+            this.btnRunCustomApp.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(435, 235);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(135, 13);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "ЗАПУСК ПРИЛОЖЕНИЙ";
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.FileName = "openFileDialog1";
+            this.openFileDialog.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog_FileOk);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnRunCustomApp);
+            this.Controls.Add(this.btnRunPaint);
+            this.Controls.Add(this.btnRunCalculator);
+            this.Controls.Add(this.btnRunNotepad);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnEndProcess);
+            this.Controls.Add(this.dataGridViewDetails);
+            this.Controls.Add(this.btnSetInterval);
+            this.Controls.Add(this.textBoxInterval);
+            this.Controls.Add(this.listBoxProcesses);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewDetails)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBoxProcesses;
+        private System.Windows.Forms.TextBox textBoxInterval;
+        private System.Windows.Forms.Button btnSetInterval;
+        private System.Windows.Forms.DataGridView dataGridViewDetails;
+        private System.Windows.Forms.Button btnEndProcess;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnRunNotepad;
+        private System.Windows.Forms.Button btnRunCalculator;
+        private System.Windows.Forms.Button btnRunPaint;
+        private System.Windows.Forms.Button btnRunCustomApp;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+    }
+}
+
---
+++ ProcessViewApp/Form1.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProcessViewApp
+{
+    public partial class Form1 : Form
+    {
+        private System.Threading.Timer timer;
+        private int interval = 10000; // Интервал обновления по умолчанию (в миллисекундах)
+        public Form1()
+        {
+            InitializeComponent();
+            LoadProcesses();
+            dataGridViewDetails.Columns.Add("Property", "Property");
+            dataGridViewDetails.Columns.Add("Value", "Value");
+            listBoxProcesses.SelectedIndexChanged += listBoxProcesses_SelectedIndexChanged;
+            //Запускаем таймер для обновления процессов с указанным интервалом
+            timer = new System.Threading.Timer(UpdateProcesses, null, interval, interval);
+        }
+        private int GetProcessCount(string processName)
+        {
+
+            Process[] processes = Process.GetProcessesByName(processName);
+            return processes.Length;
+        }
+        private void LoadProcesses()
+        {
+            Process[] processes = Process.GetProcesses();
+            foreach (Process process in processes)
+            {
+                listBoxProcesses.Items.Add($"{process.ProcessName} (ID: {process.Id})");
+            }
+        }
+
+        private void UpdateProcesses(object state)
+        {
+            //Очищаем список процессов и обновляем его
+            listBoxProcesses.Invoke((MethodInvoker)delegate
+            {
+                listBoxProcesses.Items.Clear();
+                LoadProcesses();
+            });
+        }
+
+        private void listBoxProcesses_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listBoxProcesses.SelectedIndex != -1)
+            {
+                Process selectedProcess = Process.GetProcesses()[listBoxProcesses.SelectedIndex];
+                ShowProcessDetails(selectedProcess);
+            }
+        }
+
+        private void ShowProcessDetails(Process process)
+        {
+            dataGridViewDetails.Rows.Clear();
+
+            dataGridViewDetails.Rows.Add("Идентификатор процесса", process.Id);
+            dataGridViewDetails.Rows.Add("Время старта", process.StartTime.ToString());
+            dataGridViewDetails.Rows.Add("Общее время CPU", process.TotalProcessorTime.ToString());
+            dataGridViewDetails.Rows.Add("Количество потоков", process.Threads.Count);
+            int count = GetProcessCount(process.ProcessName);
+            dataGridViewDetails.Rows.Add("Количество копий проесса", count);
+
+            // Отображаем информацию о процессе в DataGridView
+            dataGridViewDetails.AutoResizeColumns();
+        }
+        private void btnSetInterval_Click_1(object sender, EventArgs e)
+        {
+            // Устанавливаем новый интервал обновления из текстового поля
+            if (int.TryParse(textBoxInterval.Text, out int newInterval) && newInterval > 0)
+            {
+                interval = newInterval;
+                timer.Change(0, interval); // Изменяем интервал таймера
+            }
+            else
+            {
+                MessageBox.Show("Пожалуйста, введите корректный интервал в миллисекундах.");
+            }
+        }
+        private void btnEndProcess_Click_1(object sender, EventArgs e)
+        {
+            if (listBoxProcesses.SelectedIndex != -1)
+            {
+                Process[] processes = Process.GetProcesses();
+                Process selectedProcess = processes[listBoxProcesses.SelectedIndex];
+
+                if (!selectedProcess.HasExited)
+                {
+                    try
+                    {
+                      //  selectedProcess.Kill(); // Завершение выбранного процесса
+                        MessageBox.Show("Процесс успешно завершен.");
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show($"Ошибка при завершении процесса: {ex.Message}");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Выбранный процесс уже завершен.");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Пожалуйста, выберите процесс для завершения.");
+            }
+        }
+        //обработчик события для кнопки запуска блокнота
+        private void btnRunNotepad_Click_1(object sender, EventArgs e)
+        {
+            try
+            {
+                Process.Start("notepad.exe");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка при запуске блокнота: {ex.Message}");
+            }
+        }
+
+        //обработчик события для кнопки запуска калькулятора
+        private void btnRunCalculator_Click_1(object sender, EventArgs e)
+        {
+            try
+            {
+                Process.Start("calc.exe");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка при запуске калькулятора: {ex.Message}");
+            }
+        }
+
+        //обработчик события для кнопки запуска Paint
+        private void btnRunPaint_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Process.Start("mspaint.exe");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка при запуске Paint: {ex.Message}");
+            }
+        }
+
+        //обработчик события для кнопки запуска пользовательского приложения
+        private void btnRunCustomApp_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Executable Files (*.exe)|*.exe|All Files (*.*)|*.*";
+            openFileDialog.Title = "Выберите исполняемый файл";
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string selectedApp = openFileDialog.FileName;
+                try
+                {
+                    Process.Start(selectedApp);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Ошибка при запуске выбранного приложения: {ex.Message}");
+                }
+            }
+        }
+
+        private void openFileDialog_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+    }
+}
